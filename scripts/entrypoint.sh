69b55a98628794c6a2209a9b72f9dd95c0f7092e
Boris Kuschel <kuschel@ca.ibm.com>
2018-02-01 12:25:08 -0500

    Move entrypoint to common build

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2017. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
+read -d '' vcap_services_template <<"EOF"
+    "%s": [
+        {
+            "credentials": %s,
+            %s
+        }
+    ]
+EOF
+
+printf -v VCAP_SERVICES "{$vcap_services_template}" "user-provided" "$OTC_TIAM_CLIENTS" "\"name\": \"otc-tiam-clients\""
+
+CF_INSTANCE_INDEX=$(hostname | grep -o "[[:digit:]]*$")
+
+#Avoid queue conflicts with CF instances
+CF_INSTANCE_INDEX=$((CF_INSTANCE_INDEX+100))
+
+export VCAP_SERVICES CF_INSTANCE_INDEX
+
+exec "$@"
