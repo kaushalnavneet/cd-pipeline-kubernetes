c91eb2d6c1e530a79d76f3c5152117e3146ecea0
Boris Kuschel <kuschel@ca.ibm.com>
2018-04-09 16:20:37 -0400

    Fix COMPONENT_NAME parsing

--- scripts/pipeline_build.sh
+++ scripts/pipeline_build.sh
@@ -4,7 +4,7 @@ cp -a /work cd-pipeline-kubernetes
 
 IBM_CLOUD_API=${IBM_CLOUD_API:-api.ng.bluemix.net}
 IMAGE_NAME=${IMAGE_NAME:-${IMAGE_REGISTRY}/${IMAGE_NAMESPACE}/${IDS_STAGE_NAME}}
-COMPONENT_NAME=${COMPONENT_NAME:-${IMAGE_NAME%%:*}}
+COMPONENT_NAME=${COMPONENT_NAME:-${IMAGE_NAME##*/}}
 DOCKERFILE=${DOCKERFILE:-cd-pipeline-kubernetes/docker/Dockerfile.${DOCKER_IMAGE##*:}}
 
 bx login -a ${IBM_CLOUD_API} -c ${ACCOUNT_ID} --apikey ${API_KEY}
