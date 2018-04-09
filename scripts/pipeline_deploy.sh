f23a5567017405505aad9169bef881c0cb245027
Boris Kuschel <kuschel@ca.ibm.com>
2018-04-09 13:54:33 -0400

    Include defautls for env vars

--- scripts/pipeline_deploy.sh
+++ scripts/pipeline_deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+IBM_CLOUD_API=${IBM_CLOUD_API:-api.ng.bluemix.net}
 IMAGE_NAME=${IMAGE_NAME:-${IMAGE_REGISTRY}/${IMAGE_NAMESPACE}/${IDS_STAGE_NAME}}
+COMPONENT_NAME=${COMPONENT_NAME:-${IMAGE_NAME%%:*}}
+CHART_NAMESPACE=${CHART_NAMESPACE:-${IMAGE_NAMESPACE}}
+ENVIRONMENT=${ENVIRONMENT:-development}
 
 cp -a /work cd-pipeline-kubernetes
 bx login -a ${IBM_CLOUD_API} -c ${ACCOUNT_ID} --apikey ${API_KEY}
@@ -13,7 +17,7 @@ if ! helm list ${IDS_STAGE_NAME}; then
   if [ -z $deleted ]; then
     helm delete --purge ${IDS_STAGE_NAME}
   fi
-  helm install --name ${IDS_STAGE_NAME} ${CHART_DIR} --namespace ${CHART_NAMESPACE} --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME}
+  helm install --name ${IDS_STAGE_NAME} ${COMPONENT_NAME} --namespace ${CHART_NAMESPACE} --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${COMPONENT_NAME}
   else
     helm upgrade ${IDS_STAGE_NAME} ${CHART_DIR} --install --namespace ${CHART_NAMESPACE} --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME}
   fi
