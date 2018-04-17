6691e1caf7d87e50463c31ea6739fd30bb706192
Paul Webster <pwebster@ca.ibm.com>
2018-04-17 16:24:05 -0400

    Update to include log4j.properties

--- docker/Dockerfile.java
+++ docker/Dockerfile.java
@@ -35,6 +35,7 @@ COPY --from=build /work/warbuild/target/$warname /opt/ibm/wlp/usr/servers/defaul
 COPY --from=build /work/warbuild/serverConf/*.template /opt/ibm/wlp/usr/servers/defaultServer/
 COPY --from=build /work/warbuild/serverConf/pipeline-server /opt/ibm/wlp/
 COPY --from=build /work/warbuild/serverConf/qr.jks /opt/ibm/wlp/usr/servers/defaultServer/
+COPY --from=build /work/warbuild/serverConf/log4j.properties /opt/ibm/wlp/usr/servers/defaultServer/
 COPY --from=build /work/warbuild/jq /opt/ibm/wlp/
 COPY --from=build /work/warbuild/lib/newrelic.jar /opt/ibm/wlp/usr/servers/defaultServer/newrelic/
 #COPY --from=build /work/warbuild/serverConf/resources/security/* /opt/ibm/wlp/usr/servers/defaultServer/resources/security/
