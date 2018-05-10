019005c97cec158b759a7658cb7cce6560b2df23
Boris Kuschel <kuschel@ca.ibm.com>
2018-05-10 12:59:10 -0400

    add vulnerability advisor script

---
+++ scripts/pipeline_va.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IBM_CLOUD_API=${IBM_CLOUD_API:-api.ng.bluemix.net}
+IMAGE_NAME=${IMAGE_NAME:-${IMAGE_REGISTRY}/${IMAGE_NAMESPACE}/${IDS_STAGE_NAME}}
+COMPONENT_NAME=${COMPONENT_NAME:-${IMAGE_NAME##*/}}
+DOCKERFILE=${DOCKERFILE:-cd-pipeline-kubernetes/docker/Dockerfile.${DOCKER_IMAGE##*:}}
+
+# default value for PIPELINE_IMAGE_URL -- uncomment and customize as needed
+export PIPELINE_IMAGE_URL="${IMAGE_NAME}:${APPLICATION_VERSION}"
+echo "PIPELINE_IMAGE_URL=${PIPELINE_IMAGE_URL}"
+
+for iteration in {1..3}
+do
+  [[ $(bx cr va $PIPELINE_IMAGE_URL) == *No\ vulnerability\ scan* ]] || break
+  echo -e "A vulnerability report was not found for the specified image, either the image doesn't exist or the scan hasn't completed yet. Waiting for scan to complete.."
+  sleep 60
+done
+
+set +e
+bx cr va $PIPELINE_IMAGE_URL
+set -e
+[[ $(bx cr va $PIPELINE_IMAGE_URL) == *SAFE\ to\ deploy* ]] || { echo "ERROR: The vulnerability scan was not successful, check the output of the command and try again."; exit 1; }
