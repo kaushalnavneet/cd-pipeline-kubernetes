3d239a9692115888673feaf125756ab6afeefc96
Boris Kuschel <kuschel@ca.ibm.com>
2018-05-19 00:26:18 -0400

    Add support for vault caching

---
+++ scripts/vault-sidekick-cache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+secret=$(kubectl --token ${TOKEN} --namespace ${NAMESPACE} get secret ${VAULT_COMPONENT} -o jsonpath='{.data}')
+retVal=$?
+if [ $retVal -ne 0 ] || [ -z "$secret" ]; then
+  mkdir -p /home/vault/secrets
+  /vault-sidekick "$@"
+  kubectl --token ${TOKEN} delete secret ${VAULT_COMPONENT} || true
+  kubecmd="kubectl --token ${TOKEN} --namespace ${NAMESPACE} create secret generic ${VAULT_COMPONENT}"
+  for file in /home/vault/secrets/*.secret; do
+    [ -e "$file" ] || continue
+    kubecmd="$kubecmd --from-file=$file"
+  done
+  eval "$kubecmd"
+fi
