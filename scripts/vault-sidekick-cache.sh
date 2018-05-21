e1aa63bf41a5ef43b82c2b5e7211277b9b5864b4
Boris Kuschel <kuschel@ca.ibm.com>
2018-05-20 21:58:43 -0400

    Add vault as precedence

--- scripts/vault-sidekick-cache.sh
+++ scripts/vault-sidekick-cache.sh
@@ -2,15 +2,23 @@
 
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-secret=$(kubectl --token ${TOKEN} --namespace ${NAMESPACE} get secret ${VAULT_COMPONENT} -o jsonpath='{.data}')
+
+mkdir -p /home/vault/secrets
+/vault-sidekick "$@"
 retVal=$?
-if [ $retVal -ne 0 ] || [ -z "$secret" ]; then
-  mkdir -p /home/vault/secrets
-  /vault-sidekick "$@"
+if [ $retVal -ne 0 ]; then
+  secret=$(kubectl --token ${TOKEN} --namespace ${NAMESPACE} get secret ${VAULT_COMPONENT} -o jsonpath='{.data}')
+  retVal=$?
+  if [ $retVal -eq 0 ] && [ -z "$secret" ]; then
+    retVal=1
+  fi
+else
   kubecmd="kubectl --token ${TOKEN} --namespace ${NAMESPACE} create secret generic ${VAULT_COMPONENT}"
   for file in /home/vault/secrets/*.secret; do
     [ -e "$file" ] || continue
     kubecmd="$kubecmd --from-file=$file"
   done
   eval "$kubecmd --dry-run -o json | kubectl apply -f -"
+  retVal=$?
 fi
+exit $retVal
