8fe374752a6757cf81a8aa58edb5f0a27d754300
Boris Kuschel <kuschel@ca.ibm.com>
2018-06-01 13:01:06 -0400

    Add mmore status to VA

--- scripts/pipeline_va.sh
+++ scripts/pipeline_va.sh
@@ -22,4 +22,7 @@ do
 done
 
 echo "${BX_CR_VA}"
-[[ ${BX_CR_VA} == *SAFE* ]] || { echo "ERROR: The vulnerability scan was not successful, check the output of the command and try again."; exit 1; }
+if [[ "${BX_CR_VA}" =~ SAFE ]] || [[ "${BX_CR_VA}" =~ "NO ISSUES" ]]; then
+  echo "ERROR: The vulnerability scan was not successful, check the output of the command and try again."
+  exit 1
+fi
