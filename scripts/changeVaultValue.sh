cfeddaaf784bf716c837bffe8c76de87a5bc9764
Boris Kuschel <kuschel@ca.ibm.com>
2018-06-01 06:51:44 -0400

    Change pipeline-service to be SOC

---
+++ scripts/changeVaultValue.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Prior to execution login into an acount that has write, then set the read token VAULT_TOKEN
+
+#eg. generic/project/continuous-delivery-dev/cd-pipeline/development/pipeline-service
+CONTEXT=$1
+VAULT_KEY=$2
+VAULT_VALUE=$3
+
+vault read --format=json ${CONTEXT} | jq --arg key ${VAULT_KEY} --arg newval ${VAULT_VALUE} '.data | .[$key]=$newval' | VAULT_TOKEN= vault write ${CONTEXT} -
