b9988f422abaecd69fad6ec035c8e4f8ff44f9f1
Boris Kuschel <kuschel@ca.ibm.com>
2018-06-14 07:36:14 -0400

    Support for blue/green

--- scripts/pipeline_deploy.sh
+++ scripts/pipeline_deploy.sh
@@ -16,6 +16,9 @@ bx login -a ${IBM_CLOUD_API} -c ${ACCOUNT_ID} --apikey ${API_KEY}
 
 $(bx cs cluster-config --export ${IDS_JOB_NAME})
 
+INGRESS_SUBDOMAIN=$(bx cs cluster-get -s ${IDS_JOB_NAME} | grep -i "Ingress subdomain:" | awk '{print $3;}')
+INGRESS_SECRET=$(bx cs cluster-get -s {IDS_JOB_NAME} | grep -i "Ingress secret:" | awk '{print $3;}')
+
 tmp=$(mktemp)
 yq --yaml-output --arg stagename "${IDS_STAGE_NAME}" '. | .pipeline.fullnameOverride=$stagename | .pipeline.nameOverride=$stagename' ${COMPONENT_NAME}/values.yaml > "$tmp" && mv "$tmp" ${COMPONENT_NAME}/values.yaml
 
@@ -25,7 +28,7 @@ if ! helm list ${IDS_STAGE_NAME}; then
   if [ -z $deleted ]; then
     helm delete --purge ${IDS_STAGE_NAME}
   fi
-  helm install --name ${IDS_STAGE_NAME} ${COMPONENT_NAME} --namespace ${CHART_NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME}
+  helm install --name ${IDS_STAGE_NAME} ${COMPONENT_NAME} --namespace ${CHART_NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME} --set global.ingressSubDomain=${INGRESS_SUBDOMAIN} --set global.ingressSecret=${INGRESS_SECRET}
 else
-  helm upgrade ${IDS_STAGE_NAME} ${COMPONENT_NAME} --install --namespace ${CHART_NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME}
+  helm upgrade ${IDS_STAGE_NAME} ${COMPONENT_NAME} --install --namespace ${CHART_NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${APPLICATION_VERSION} --set pipeline.image.repository=${IMAGE_NAME} --set global.ingressSubDomain=${INGRESS_SUBDOMAIN} --set global.ingressSecret=${INGRESS_SECRET}
 fi
