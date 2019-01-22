6def0e8e6805a488921b879e1a69087bfa3e814c
Paul Webster <pwebster@ca.ibm.com>
2019-01-22 13:59:17 -0500

    Updated the values to work with nconf properly in dev.
    Issue: org-ids/roadmap#7971

--- scripts/push.sh
+++ scripts/push.sh
@@ -14,6 +14,11 @@ NAMESPACE=opentoolchain
 ENVIRONMENT=${3:-development}
 CODE_BASE=${4:-nodejs6}
 PULL_BUILDER=${5:-true}
+CLUSTER_NAME=${6:-otc-us-south-dev}
+
+#export INGRESS_SUBDOMAIN=$(bx cs cluster-get -s ${CLUSTER_NAME} | grep -i "Ingress subdomain:" | awk '{print $3;}')
+#export INGRESS_SECRET=$(bx cs cluster-get -s ${CLUSTER_NAME} | grep -i "Ingress secret:" | awk '{print $3;}')
+
 
 cat <<END > build_info.json
 {
@@ -40,9 +45,13 @@ if [  -d cd-pipeline-kubernetes ]; then
     if [ -z $deleted ]; then
       helm delete --purge ${RELEASE_NAME}
     fi
-    helm install --name ${RELEASE_NAME} ${CHART_DIR} --namespace ${NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${TAG}
+    helm install --name ${RELEASE_NAME} ${CHART_DIR} --namespace ${NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true \
+      --set pipeline.image.tag=${TAG} --set global.ingressSubDomain=${INGRESS_SUBDOMAIN} --set global.ingressSecret=${INGRESS_SECRET} \
+      --set pipeline.image.repository=registry.ng.bluemix.net/${NAMESPACE}/${IMAGE_NAME} 
   else
-    helm upgrade ${RELEASE_NAME} ${CHART_DIR} --install --namespace ${NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true --set pipeline.image.tag=${TAG}
+    helm upgrade ${RELEASE_NAME} ${CHART_DIR} --install --namespace ${NAMESPACE} --set tags.environment=false --set ${ENVIRONMENT}.enabled=true \
+      --set pipeline.image.tag=${TAG} --set global.ingressSubDomain=${INGRESS_SUBDOMAIN} --set global.ingressSecret=${INGRESS_SECRET} \
+      --set pipeline.image.repository=registry.ng.bluemix.net/${NAMESPACE}/${IMAGE_NAME} 
   fi
 else
   echo "Must clone https://github.ibm.com/org-ids/cd-pipeline-kubernetes as cd-pipeline-kubernetes into root of component directory. Then execute this script as ./cd-pipeline-kubernetes/scripts/push.sh"
