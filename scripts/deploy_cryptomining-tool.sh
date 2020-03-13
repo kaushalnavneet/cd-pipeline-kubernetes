dfbdb445316431ef576a36f1e4cae70eacc2d62b
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2020-03-13 11:26:30 -0400

    Update values path

--- scripts/deploy_cryptomining-tool.sh
+++ scripts/deploy_cryptomining-tool.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 CHART_NAMESPACE=${CHART_NAMESPACE:-opentoolchain}
 ENVIRONMENT=${ENVIRONMENT:-dev}
+DEVOPS_CONFIG=${DEVOPS_CONFIG:-devops-config}
+VALUES=${DEVOPS_CONFIG}/environments/${ENVIRONMENT}/cryptomining_values.yaml
 
 helm upgrade cryptomining-detector helm/cryptomining-detector \
   --install \
   --namespace "${CHART_NAMESPACE}" \
-  --values=environments/"${ENVIRONMENT}"/cryptomining_values.yaml \
+  --values=${VALUES}
   --timeout 600 \
   --debug
