2f6f7015053737be6272331cbd77e666547091bc
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2020-06-24 01:45:59 -0400

    Remove dry-run

--- scripts/deploy_pgbouncer_tekton.sh
+++ scripts/deploy_pgbouncer_tekton.sh
@@ -59,14 +59,11 @@ kubectl -n${CHART_NAMESPACE} create secret generic ${TARGET}-pgbouncer-secret  -
 rm -f userlist.txt
 rm -f .pgpass
 
-set -x
-
 helm init -c
 helm repo add gitlab https://charts.gitlab.io/
 helm dep up
 
 helm upgrade ${TARGET} ./charts/pgbouncer  \
-  --dry-run \
   --install \
   --set tags.environment=false \
   --set ${ENVIRONMENT}.enabled=true \
