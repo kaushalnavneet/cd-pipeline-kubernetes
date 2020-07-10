75262834d1c022306a9451003fc970a103012ec3
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2020-07-09 20:54:46 -0400

    check node version

---
+++ scripts/check_node_version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+NODE_VERSION=2.18.2
+NAMESPACE="opentoolchain"
+OLDIFS=$IFS
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+checkNodeVersion() {
+    local namespace=$1
+    local prefix=$2
+
+	pods=()
+	while IFS='' read -r line; do pods+=("$line"); done < <(kubectl get pods -n "${namespace}" | grep $prefix | grep Running  | awk '{print $1}')
+
+    echo "Checking ${pods[0]}"
+    status=$(kubectl -n "${namespace}" exec ${pods[0]} -- node --version | grep $NODE_VERSION)
+    if [[ -z $status ]]; then
+        echo -e "${RED}Wrong node version for ${pods[0]}${NC}"
+    fi
+}
+
+IFS=','
+apps="blade-pipeline-broker,otc-github-relay-pub,otc-github-relay-sub,pipeline-artifact-repository-service,pipeline-consumption,pipeline-event-service,pipeline-log-service,pipeline-ui,private-worker-service,pipeline-support-service"
+#apps="pipeline-support-service"
+
+for app in $apps
+do
+    checkNodeVersion $NAMESPACE $app
+done
