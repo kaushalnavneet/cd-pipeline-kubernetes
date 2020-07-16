65da4db26a5d49139529195018e93e1a85807745
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2020-07-16 15:13:50 -0400

    New UBI files

---
+++ scripts/entrypoint.nodejsubi.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2020. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
+if [ -d /etc/secrets ]; then
+    for file in /etc/secrets/*.secret; do
+        [ -e "$file" ] || continue
+        eval "$(/home/node/jq -r  '. | to_entries | .[] | "export " + .key + "=" + ( .value|if (type|. != "string") then tostring else .|tojson end)' < $file)"
+    done
+fi
+
+read -d '' vcap_services_template <<"EOF"
+    "%s": [
+        {
+            "credentials": {
+	       "pipeline":{  
+               	 "initial":null,
+                 "secret":"%s"
+               }
+	    },
+            %s
+        }
+    ]
+EOF
+
+printf -v VCAP_SERVICES "{$vcap_services_template}" "user-provided" "$vcap_pipeline_secret" "\"name\": \"otc-tiam-clients\""
+
+export VCAP_SERVICES
+
+export HOST_INSTANCE=${HOSTNAME##*-} 
+
+CF_INSTANCE_INDEX=$(hostname | grep -o "[[:digit:]]*$")
+
+#Avoid queue conflicts with CF instances
+CF_INSTANCE_INDEX=$((CF_INSTANCE_INDEX+100))
+
+export VCAP_SERVICES CF_INSTANCE_INDEX
+
+exec "$@"
