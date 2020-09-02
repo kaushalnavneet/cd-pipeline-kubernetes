0a2c76a076e1597902e46c313067fcfbdd679edc
Olivier Thomann <olivier@MacBook-Pro.local>
2020-09-02 16:46:17 -0400

    Add ubi travis-worker

---
+++ docker/docker_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y http://mirror.centos.org/centos/7/os/x86_64/Packages/libseccomp-2.3.1-4.el7.x86_64.rpm http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.119.2-1.911c772.el7_8.noarch.rpm
+yum install docker-ce -y
+systemctl start docker.service
