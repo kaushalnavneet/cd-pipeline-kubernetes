4c31462e7eff46fe5d7fab7f1c7969e36faf3477
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2020-10-21 19:45:26 -0400

    adjust docker pull script

--- docker/docker_ubi/docker_pull_start.sh
+++ docker/docker_ubi/docker_pull_start.sh
@@ -20,33 +20,40 @@ docker info >/dev/null 2>&1
 
 # get list of all existing images
 docker images >images.txt
-
+cat images.txt
 docker pull ${WORKER_LEGACY_IMAGE}
 docker tag ${WORKER_LEGACY_IMAGE} ibm_devops_services/worker_base:latest
 docker pull ${WORKER_DIND_IMAGE}
 docker tag ${WORKER_DIND_IMAGE} ibm_devops_services/worker_dind:latest
 
-pullImage ${WORKER_LEGACY_IMAGE} 1.0
-pullImage ${WORKER_20_BASE_IMAGE} 2.0
-pullImage ${WORKER_21_BASE_IMAGE} 2.1
-pullImage ${WORKER_22_BASE_IMAGE} 2.2
-pullImage ${WORKER_23_BASE_IMAGE} 2.3
-pullImage ${WORKER_24_BASE_IMAGE} 2.4
-pullImage ${WORKER_25_BASE_IMAGE} 2.5
-pullImage ${WORKER_26_BASE_IMAGE} 2.6
-pullImage ${WORKER_27_BASE_IMAGE} 2.7
-pullImage ${WORKER_28_BASE_IMAGE} 2.8
-pullImage ${WORKER_29_BASE_IMAGE} 2.9
+pullImage ${WORKER_LEGACY_IMAGE} 1.0 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_20_BASE_IMAGE} 2.0 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_21_BASE_IMAGE} 2.1 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_22_BASE_IMAGE} 2.2 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_23_BASE_IMAGE} 2.3 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_24_BASE_IMAGE} 2.4 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_25_BASE_IMAGE} 2.5 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_26_BASE_IMAGE} 2.6 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_27_BASE_IMAGE} 2.7 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_28_BASE_IMAGE} 2.8 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
+pullImage ${WORKER_29_BASE_IMAGE} 2.9 ${WORKER_CURATED_IMAGES} ${WORKER_TRAVIS_REGISTRY_URL}
 
 function pullImage {
   local vbi_name=$1
   local version=$2
-  local base_image_name=`echo ${WORKER_CURATED_IMAGES} | tr ',' $'\n' | grep ${version} | sed -e 's#.*=\(\)#\1#'`
+  local curated_images=$3
+  local registry_url=$4
+  echo "vbi name: ${vbi_name}"
+  echo "vbi version: ${version}"
+  echo "curated_images: ${curated_images}"
+  echo "registry_url: ${registry_url}"
+
+  local base_image_name=`echo ${curated_images} | tr ',' $'\n' | grep ${version} | sed -e 's#.*=\(\)#\1#'`
   local base_image_tag=`echo ${vbi_name} | sed -e 's#.*:\(\)#\1#'`
-  exits=$(cat images.txt | grep ${WORKER_TRAVIS_REGISTRY_URL}/${base_image_name} | grep ${base_image_tag})
+  exits=$(cat images.txt | grep ${registry_url}/${base_image_name} | grep ${base_image_tag})
   if [[ $? -ne 0 ]]; then
     docker pull ${vbi_name}
-    docker tag ${vbi_name} ${WORKER_TRAVIS_REGISTRY_URL}/${base_image_name}:${base_image_tag}
-    docker push ${WORKER_TRAVIS_REGISTRY_URL}/${base_image_name}:${base_image_tag}
+    docker tag ${vbi_name} ${registry_url}/${base_image_name}:${base_image_tag}
+    docker push ${registry_url}/${base_image_name}:${base_image_tag}
   fi
 }
