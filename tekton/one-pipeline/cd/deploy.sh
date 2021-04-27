e7614f847c562cea2d662b32fee537dd5370564f
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2021-04-27 14:27:48 -0400

    debug deploy script

--- tekton/one-pipeline/cd/deploy.sh
+++ tekton/one-pipeline/cd/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 echo "deploy"
 env
+cd "${WORKSPACE}"
 ls -la
-pwd
-cd /workspace/app
-ls -la
-#jq -rc '.[]' /artifacts/deployment-delta-list.json
+jq -rc '.[]' "${WORKSPACE}/${DEPLOYMENT_DELTA_PATH}"
