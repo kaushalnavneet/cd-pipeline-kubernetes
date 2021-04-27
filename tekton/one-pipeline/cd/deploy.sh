b41dac50fee1ca158619dffa20be17df8123204d
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2021-04-27 18:28:29 -0400

    deploy to 3 clusters

--- tekton/one-pipeline/cd/deploy.sh
+++ tekton/one-pipeline/cd/deploy.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 source "${WORKSPACE}/${ONE_PIPELINE_CONFIG_DIRECTORY_NAME}/tekton/one-pipeline/cd/helpers.sh"
-echo ">>>>>>>>>>>>>>>>>>>"
-env
-echo ">>>>>>>>>>>>>>>>>>>"
-cd "${WORKSPACE}"
-ls -la
-echo ">>>>>>>>>>>>>>>>>>>"
 export CLUSTER_NAMESPACE=$(cat /config/cluster-namespace)
-export API_KEY=$(cat /config/API_KEY_1416501)
+export CLUSTER_NAME1=$(cat /config/cluster_name1)
+export CLUSTER_NAME2=$(cat /config/cluster_name2)
+export CLUSTER_NAME3=$(cat /config/cluster_name3)
+export API_KEY=$(cat /config/ibmcloud-api-key)
 declare -a apps=($(jq -rc '.[]' "${WORKSPACE}/${DEPLOYMENT_DELTA_PATH}"))
 
 for app in "${apps[@]}"; do
     if [[ "$app" != *"_image" ]]; then
         echo "deploy ${app}"
-        deployComponent "${app}" "otc-osa21-prod" "${CLUSTER_NAMESPACE}" "${REGION}" "${REGION}"
+        deployComponent "${app}" "${CLUSTER_NAME1}" "${CLUSTER_NAMESPACE}" "${REGION}" "${REGION}"
+        deployComponent "${app}" "${CLUSTER_NAME2}" "${CLUSTER_NAMESPACE}" "${REGION}" "${REGION}"
+        deployComponent "${app}" "${CLUSTER_NAME3}" "${CLUSTER_NAMESPACE}" "${REGION}" "${REGION}"
     fi
 done
