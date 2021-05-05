50bf12ccee538942de2695e233512583522aaeed
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2021-05-05 16:33:05 -0400

    Fix cluster names

--- tekton/one-pipeline/cd/deploy.sh
+++ tekton/one-pipeline/cd/deploy.sh
@@ -6,15 +6,16 @@ chmod 700 get_helm.sh
 
 source "${WORKSPACE}/${ONE_PIPELINE_CONFIG_DIRECTORY_NAME}/tekton/one-pipeline/cd/helpers.sh"
 export CLUSTER_NAMESPACE=$(cat /config/cluster-namespace)
-export CLUSTER_NAME1=$(cat /config/cluster_name1)
-export CLUSTER_NAME2=$(cat /config/cluster_name2)
-export CLUSTER_NAME3=$(cat /config/cluster_name3)
+
 export API_KEY=$(cat /config/ibmcloud-api-key)
 declare -a apps=($(jq -rc '.[]' "${WORKSPACE}/${DEPLOYMENT_DELTA_PATH}"))
 
 for app in "${apps[@]}"; do
     if [[ "$app" != *"_image" ]]; then
         echo "deploy ${app}"
+        export CLUSTER_NAME1=$(cat /config/cluster_name1)
+        export CLUSTER_NAME2=$(cat /config/cluster_name2)
+        export CLUSTER_NAME3=$(cat /config/cluster_name3)
         if [[ "$app" == "travis-worker" ]]; then
             export CLUSTER_NAME1="$(echo $CLUSTER_NAME1 | cut -d - -f1)-pw-$(echo $CLUSTER_NAME1 | cut -d - -f2-3)"
             export CLUSTER_NAME2="$(echo $CLUSTER_NAME2 | cut -d - -f1)-pw-$(echo $CLUSTER_NAME2 | cut -d - -f2-3)"
