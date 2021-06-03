5796a52678968be1d6a323961cfe8ba512f4d8e1
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2021-06-03 17:07:54 -0400

    add debug

--- scripts/deploy_cryptomining-tool-no-helm.sh
+++ scripts/deploy_cryptomining-tool-no-helm.sh
@@ -44,9 +44,12 @@ wget --no-check-certificate https://github.com/stedolan/jq/releases/download/jq-
 
 echo "---"
 # do substitution inside json files located in releng folder
+echo "--- 1"
 cat releng/crypto_deploy.json | jq --arg CLUSTER_NAME ${CLUSTER_NAME} '(.spec.template.spec.containers[].env[] | select(.name == "CLUSTER_NAME") | .value) = "$CLUSTER_NAME"' > releng/1.json
-cat releng/1.json | jq jq --arg IMAGE_NAME ${IMAGE_NAME} '(.spec.template.spec.containers[].image) = "$IMAGE_NAME"' > releng/2.json
-cat releng/2.json | jq jq --arg SCHEDULE ${SCHEDULE} '(.spec.template.spec.containers[].env[] | select(.name == "SCHEDULE") | .value) = "$SCHEDULE"' > releng/final_crypto_deploy.json
+echo "--- 2"
+cat releng/1.json | jq --arg IMAGE_NAME ${IMAGE_NAME} '(.spec.template.spec.containers[].image) = "$IMAGE_NAME"' > releng/2.json
+echo "--- 3"
+cat releng/2.json | jq --arg SCHEDULE ${SCHEDULE} '(.spec.template.spec.containers[].env[] | select(.name == "SCHEDULE") | .value) = "$SCHEDULE"' > releng/final_crypto_deploy.json
 rm releng/crypto_deploy.json
 rm releng/1.json
 rm releng/2.json
