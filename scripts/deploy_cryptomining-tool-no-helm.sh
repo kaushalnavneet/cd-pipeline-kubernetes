bab094e8a647a1faf0e52eac54030d256cc236f7
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2021-06-03 17:13:51 -0400

    fix substitution

--- scripts/deploy_cryptomining-tool-no-helm.sh
+++ scripts/deploy_cryptomining-tool-no-helm.sh
@@ -45,21 +45,21 @@ wget --no-check-certificate https://github.com/stedolan/jq/releases/download/jq-
 echo "---"
 # do substitution inside json files located in releng folder
 echo "--- 1"
-cat releng/crypto_deploy.json | jq --arg CLUSTER_NAME ${CLUSTER_NAME} '(.spec.template.spec.containers[].env[] | select(.name == "CLUSTER_NAME") | .value) = "$CLUSTER_NAME"' > releng/1.json
+cat releng/crypto_deploy.json | jq --arg CLUSTER_NAME ${CLUSTER_NAME} '(.spec.template.spec.containers[].env[] | select(.name == "CLUSTER_NAME") | .value) = $CLUSTER_NAME' > releng/1.json
 echo "--- 2"
-cat releng/1.json | jq --arg IMAGE_NAME ${IMAGE_NAME} '(.spec.template.spec.containers[].image) = "$IMAGE_NAME"' > releng/2.json
+cat releng/1.json | jq --arg IMAGE_NAME ${IMAGE_NAME} '(.spec.template.spec.containers[].image) = $IMAGE_NAME' > releng/2.json
 echo "--- 3"
-cat releng/2.json | jq --arg SCHEDULE ${SCHEDULE} '(.spec.template.spec.containers[].env[] | select(.name == "SCHEDULE") | .value) = "$SCHEDULE"' > releng/final_crypto_deploy.json
+cat releng/2.json | jq --arg SCHEDULE ${SCHEDULE} '(.spec.template.spec.containers[].env[] | select(.name == "SCHEDULE") | .value) = $SCHEDULE' > releng/final_crypto_deploy.json
 rm releng/crypto_deploy.json
 rm releng/1.json
 rm releng/2.json
 cat releng/final_crypto_deploy.json
 echo "---"
-cat releng/crypto_serviceaccount.json | jq --arg CHART_NAMESPACE ${CHART_NAMESPACE} '(.metadata.namespace) = "$CHART_NAMESPACE"' > releng/final_crypto_serviceaccount.json
+cat releng/crypto_serviceaccount.json | jq --arg CHART_NAMESPACE ${CHART_NAMESPACE} '(.metadata.namespace) = $CHART_NAMESPACE' > releng/final_crypto_serviceaccount.json
 rm releng/crypto_serviceaccount.json
 cat releng/final_crypto_serviceaccount.json
 echo "---"
-cat releng/crypto_rolebinding.json | jq --arg CHART_NAMESPACE ${CHART_NAMESPACE} '(.subjects[].namespace) = "$CHART_NAMESPACE"' > releng/final_crypto_rolebinding.json
+cat releng/crypto_rolebinding.json | jq --arg CHART_NAMESPACE ${CHART_NAMESPACE} '(.subjects[].namespace) = $CHART_NAMESPACE' > releng/final_crypto_rolebinding.json
 rm releng/crypto_rolebinding.json
 cat releng/final_crypto_rolebinding.json
 echo "---"
